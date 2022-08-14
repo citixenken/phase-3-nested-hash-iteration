f2f91e0be178d33aa311fc42cf6b93e8903e9957
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-14 15:35:38 +0300

    commit tree 0164339a21fa0f66bb38e876ace09bff66dfa273

--- contacts.rb
+++ contacts.rb
@@ -18,8 +18,30 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    # binding.pry 
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attrib, data|
+        # binding.pry
+        if attrib == :favorite_ice_cream_flavors
+          # binding.pry
+          data.delete_if { |ice_cream| ice_cream == "strawberry" }
+        end
+      end
+    end
+  end
 end
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     if person == "Freddy Mercury"
+#       contact_details_hash.each do |attribute, data|
+#         if attribute == :favorite_ice_cream_flavors
+#           data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#         end
+#       end
+#     end
+#   end
+# end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
